d900a3bd58a079255bedfa23e0367b76a31c8352
Tim Peel <tim.peel@equalexperts.com>
2026-07-02 11:57:17 +0100

    Add translation tooling to PR checks and exclude from vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     clearMocks: true,
+    exclude: [...configDefaults.exclude, 'scripts/translations/**'],
     coverage: {
       provider: 'v8',
       reportsDirectory: './coverage',
